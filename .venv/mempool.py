760ad1e4765c2936490d2f319fb77d3e6e5db254
larkri <kristoffer.larsson@outlook.com>
2025-04-21 23:42:30 +0200

    Update .gitignore to exclude .venv

--- .venv/mempool.py
+++
@@ -1,9 +0,0 @@
-mempool = []
-
-def add_to_mempool(transaction):
-    # Lägg till en ny transaktion i mempoolen
-    mempool.append(transaction)
-
-def get_mempool():
-    # Hämta alla transaktioner från mempoolen
-    return mempool
